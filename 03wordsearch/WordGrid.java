29ea74a9bbc9acb5863230672fc12ed8fb5843cd
JaredBeh <jaredbeh@outlook.com>
2014-11-25 01:18:25 -0500

    Let this be a reminder to me: dont write y when you mean x. Also, make sure you are in the right git repo. I think I should sleep now. It took way too long for me to find that bug.

--- 03wordsearch/WordGrid.java
+++ 03wordsearch/WordGrid.java
@@ -1,10 +1,14 @@
+import java.util.*;
+import java.io.File;
+import java.io.FileNotFoundException;
 public class WordGrid{
     private char[][]data;
+    private ArrayList<String> theWords=new ArrayList<String>(30);
     public int rows,cols;
-    public WordGrid(int rows,int cols){
-	data=new char[rows][cols];
-	this.rows=rows;
-	this.cols=cols;
+    public WordGrid(int numrows,int numcols){
+	data=new char[numrows][numcols];
+	rows=numrows;
+	cols=numcols;
     }
     private void clear(){
 	for (int i=0;i<rows;i++){
@@ -30,113 +34,44 @@ public class WordGrid{
 	}
 	return ans;
     }
-    public boolean addWordUpLeft(String word,int row,int col){
-	return addWordDownRight(reverseString(word),1+row-word.length(),1+col-word.length());
-    }
-    public boolean addWordUp(String word,int row,int col){
-	return addWordVertical(reverseString(word),1+row-word.length(),col);
-    }
-    public boolean addWordBackwards(String word,int row,int col){
-	return addWordHorizontal(reverseString(word),row,1+col-word.length());
-    }
-    public boolean addWordHorizontal(String word,int row,int col){
+    public boolean add(String word,int row,int col,int dx,int dy){
+	System.out.println(row+(dy*word.length()));
 	boolean ans=!(word.length()==0) &&
 	    row>=0 &&
 	    col>=0 &&
-	    row<=rows &&
-	    cols>=(col+word.length());
+	    (dx==1||dx==0||dx==-1) &&
+	    (dy==1||dy==0||dy==-1) &&
+	    (cols>=col+(dx*word.length())||0<=col+(dx*word.length())) &&
+	    (rows>=(row+(dy*word.length()))||0<=row+(dy*word.length()));
 	if (!ans){
 	    return false;
 	}
-	for (int i=col;i<col+word.length();i++){
-	    if (data[row][i]==(' ') || data[row][i]==word.charAt(i-col)){
-		continue;
-	    }
-	    else{
-		return false;
-	    }
-	}
-	for (int i=col;i<col+word.length();i++){
-	    data[row][i]=word.charAt(i-col);
-	}
-	return ans;
-	    
-    }
-    public boolean addWordVertical(String word,int row,int col){
-	boolean ans=!(word.length()==0) &&
-	    row>=0 &&
-	    col>=0 &&
-	    col<=cols &&
-	    rows>=(row+word.length());
-	if (!ans){
-	    return false;
-	}
-	for (int i=row;i<row+word.length();i++){
-	    if (data[i][col]==(' ') || data[i][col]==word.charAt(i-row)){
-		continue;
-	    }
-	    else{
-		return false;
-	    }
-	}
-	for (int i=row;i<row+word.length();i++){
-	    data[i][col]=word.charAt(i-row);
-	}
-	return ans;
-	    
-    }
-    public boolean addWordDownRight(String word,int row,int col){
-	boolean ans=!(word.length()==0) &&
-	    row>=0 &&
-	    col>=0 &&
-	    cols>=(col+word.length()) &&
-	    rows>=(row+word.length());
-	if (!ans){
-	    return false;
-	}
+	char[][]data2=data;
 	int col2=col;
-	for (int i=row;i<row+word.length();i++){
-	    if (data[i][col2]==(' ') || data[i][col2]==word.charAt(i-row)){
-		col2++;
-		continue;
-	    }
-	    else{
+	for (int i=0;i<word.length();i++){
+	    if (!(data[row+(dy*i)][col2]==(word.charAt(i))) &&
+		(data[row+(dy*i)][col2]!=' ')){
+		data=data2;
 		return false;
 	    }
+	    data[row+dy*i][col2]=word.charAt(i);
+	    col2=col2+dx;
 	}
-	for (int i=row;i<row+word.length();i++){
-	    data[i][col]=word.charAt(i-row);
-	    col++;
+	return true;
+    }
+    public void wordBankReader() throws FileNotFoundException {
+	File text=new File("wordbank.txt");
+	Scanner scnr = new Scanner(text);
+	while (scnr.hasNextLine()){
+	    if (!(theWords.add(scnr.nextLine()))){
+		break;
+	    }
 	}
-	return ans;
-	    
     }
     public static void main(String args[]){
-	WordGrid a = new WordGrid(4,4);
-	a.data[0][0]='a';
-	a.data[0][1]='b';
-	a.data[2][3]='c';
-	a.data[1][3]='d';
-	System.out.println(a);
+	WordGrid a = new WordGrid(10,10);
 	a.clear();
+        System.out.println(a.add("test",2,5,-1,0));
 	System.out.println(a);
-	a.addWordHorizontal("Sup",0,1);
-	System.out.println(a);
-	System.out.println(a.addWordUpLeft("dac",3,3));
-	System.out.println(a);
-	/*a.addWordHorizontal("dlc",1,1);
-	a.addWordHorizontal("sho",1,0);
-	System.out.println(a);
-	a.clear();
-	a.addWordVertical("ones",0,0);
-	a.addWordVertical("da",1,0);
-	a.addWordHorizontal("ya",1,1);
-	a.addWordVertical("bal",0,2);
-	System.out.println(a);
-	System.out.println(a.addWordVertical("a",0,0));
-	if (a.addWordVertical("test",0,3)){
-	    System.out.println("worked");
-	}
-	System.out.println(a);*/
     }
 }
