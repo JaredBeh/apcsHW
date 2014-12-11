542ef2d0f30d45cd847ea338b1f43e970d007484
JaredBeh <jaredbeh@outlook.com>
2014-12-10 21:19:25 -0500

    yes it works and it isnt after midnight what is this

--- 05GUICtoF/Window2.java
+++ 05GUICtoF/Window2.java
@@ -4,10 +4,9 @@ import java.awt.event.*;
 public class Window2 extends JFrame implements ActionListener{
     private Container pane;
 
-    private JButton b,clear;
-    private JLabel l;
-    private JTextField t;
-    private JCheckBox c;
+    private JButton far,cel;
+    private JLabel answer;
+    private JTextField text;
     public Window2() {
 	this.setTitle("My first GUI");
 	this.setSize(600,400);
@@ -17,26 +16,38 @@ public class Window2 extends JFrame implements ActionListener{
 
 	pane = this.getContentPane();
 	pane.setLayout(new FlowLayout());
-
-	b = new JButton("Do Nothing");
-	l = new JLabel("This is AWESOME! (lies)",null,JLabel.CENTER);
-	t = new JTextField(12);
-	c = new JCheckBox("Overclock!!!");
-	pane.add(l);
-	pane.add(b);
-	pane.add(t);
-	pane.add(c);
-	b.addActionListener(this);
-	b.setActionCommand("taptap");
+	answer=new JLabel("");
+	far = new JButton("Convert to Farenheit");
+	cel = new JButton("Convert to Celsius");
+	text = new JTextField(12);
+	pane.add(far);
+	pane.add(cel);
+	pane.add(text);
+	pane.add(answer);
+	far.addActionListener(this);
+	far.setActionCommand("tofar");
+	cel.addActionListener(this);
+	cel.setActionCommand("tocel");
     }
     public void actionPerformed(ActionEvent e){
 	String s=e.getActionCommand();
-	if (s=="taptap"){
-	    
+	String theInput=text.getText();
+	try{
+	    Double ans=Double.valueOf(theInput);
+	    if (s=="tofar"){
+		ans=ans*9/5+32;
+		answer.setText(""+ans+" F");
+	    }
+	    else if(s=="tocel"){
+		ans=(ans-32)*5/9;
+		answer.setText(""+ans+" C");
+	    }
+	}catch(NumberFormatException n){
+	    System.out.println("Please enter a number");
 	}
     }
     public static void main(String[] args) {
-        Window2 g = new Window2();
+	Window2 g = new Window2();
         g.setVisible(true);
     }
 }
